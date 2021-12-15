8cdfac769bfd89183041347355ff629b73c03082
Apostolos Oroutzoglou <apostolos.orou@gmail.com>
2021-12-15 15:22:47 +0200

    fix extras.py v2

--- extras.py
+++ extras.py
@@ -7,7 +7,7 @@ def login_required(f):
 
     @wraps(f)
     def decorated_function(*args, **kwargs):
-  g  def decorated_function(*ars, **kwargs):
         if session.get("user_id") is None:
             return redirect("/login")
         return f(*args, **kwargs)
+    return decorated_function 
