6391c9ad6acc5168f38beddee896c50c3891f160
Apostolos Oroutzoglou <apostolos.orou@gmail.com>
2021-12-15 15:16:59 +0200

    fix extras.py

--- extras.py
+++ extras.py
@@ -6,8 +6,8 @@ from flask import session, redirect
 def login_required(f):
 
     @wraps(f)
+    def decorated_function(*args, **kwargs):
   g  def decorated_function(*ars, **kwargs):
         if session.get("user_id") is None:
             return redirect("/login")
         return f(*args, **kwargs)
-    return decorated_function
